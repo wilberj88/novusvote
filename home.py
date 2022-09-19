31dc590f68969b3579d365e617d8f8bdc8a87ca4
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-19 09:57:18 -0500

    Create home.py

---
+++ home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Vote", page_icon=":battery:")
+
+st.title('Novus Vote 🗳️')
+st.header("Sistemas Inteligentes para Conseguir Votos")
+
+st.write("Bienvenidos al futuro electoral 👋")
+
+st.markdown(
+  """
+  En esta web encontrarás:
+  - 🔎 _    Diagnóstico de Necesidades y Sentimientos territoriales
+  - 🛒 _    Recomendación de Propuestas y Mensajes territoriales
+  - ✍️ _     Propuesta Inteligente de Contrato
+  
+  Todo lo anterior basado en:
+  - Tecnología para Escucha Social Digital
+  - Tecnología para Análisis de Sentimiento
+  
+  EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
+  """
+)
