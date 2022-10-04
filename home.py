ec35e3318ac26d57bb9e0e4e07a1aadf5d87b6a8
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-04 10:18:17 -0500

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
-st.set_page_config(layout="wide", page_title="Novus Vote", page_icon=":battery:")
+st.set_page_config(layout="wide", page_title="Novus Vote", page_icon="🗳️")
 
 st.title('Novus Vote 🗳️')
 st.header("Sistemas Inteligentes para Conseguir Votos")
