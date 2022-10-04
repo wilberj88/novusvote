b6d13e6600e05ba00580b2c1ccb69e2117d113dd
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-04 10:23:52 -0500

    Create mensajes_recomendados.py

---
+++ pages/mensajes_recomendados.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Novus Vote 🗳️')
+st.header("Sistema de Recomendación de Mensajes Territoriales 🎯")
+
+st.write("Configura los parámetros para unos mensajes más potentes ⚙️")
+
