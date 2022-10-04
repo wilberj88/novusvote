ecfeedf5e6a68bba7ef0434a650dc48d6b7444fc
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-04 11:14:51 -0500

    Update necesidad_territorial.py

--- pages/necesidad_territorial.py
+++ pages/necesidad_territorial.py
@@ -1,1 +1,13 @@
-st.radio('¿Cuál es tu fuente principal de energia?', ['Agua', 'Carbon', 'Combustible', 'Termoelectrica', 'Solar', 'Viento'], key="fuente")
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Vote", page_icon="🗳️")
+
+st.title('Novus Vote 🗳️')
+st.header("Análisis de Necesidades Territoriales")
+
+st.write("Selecciona la tipología de necesidad:")
+
+st.radio('Elige una', ['Acceso a Servicios Públicos Básicos', 'Empleo', 'Internet', 'Educación', 'Salud', 'Movilidad'], key="fuente")
