0bf892a58f15457a5c2ff435ebd32de534a62c90
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-04 11:15:23 -0500

    Update necesidad_territorial.py

--- pages/necesidad_territorial.py
+++ pages/necesidad_territorial.py
@@ -8,6 +8,4 @@ st.set_page_config(layout="wide", page_title="Novus Vote", page_icon="🗳️")
 st.title('Novus Vote 🗳️')
 st.header("Análisis de Necesidades Territoriales")
 
-st.write("Selecciona la tipología de necesidad:")
-
-st.radio('Elige una', ['Acceso a Servicios Públicos Básicos', 'Empleo', 'Internet', 'Educación', 'Salud', 'Movilidad'], key="fuente")
+st.radio('Selecciona la tipología de necesidad:', ['Acceso a Servicios Públicos Básicos', 'Empleo', 'Internet', 'Educación', 'Salud', 'Movilidad'], key="fuente")
