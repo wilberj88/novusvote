2d3ebfb337f3b659c207f3954349f0e0c368c299
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-04 10:27:26 -0500

    Update lideres.py

--- pages/lideres.py
+++ pages/lideres.py
@@ -12,3 +12,13 @@ picture = st.camera_input("1. Identifique su rostro")
 if picture:
     st.image(picture)
     st.write('Analizaremos el sentimiento de estas imagenes y te informaremos')
+
+    
+    a = st.text_input('¿Cuál es el nombre de la competencia?')
+
+b = st.text_input('¿Cuál es su pagina web?')
+
+c = st.text_input('¿Cuál es el producto o servicio de tu mayor interés?')
+
+if a and b and c:
+  st.write('Estamos preparando un Mando con el análisis de la competencia que genera <<',a, '>>, a partir de su web <<', b, '>> y en especial para el producto <<', c, '>>')
