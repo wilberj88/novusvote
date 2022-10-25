c8db1f1c24de1a9b06f1185f055ec288f714e5e9
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-25 07:19:57 -0500

    Update sistema_politico.py

--- pages/sistema_politico.py
+++ pages/sistema_politico.py
@@ -7,3 +7,7 @@ st.set_page_config(layout="wide", page_title="Novus Vote", page_icon="🗳️")
 
 st.title('Novus Vote 🗳️')
 st.header("Sistema Inteligente de Gestión Política")
+
+
+
+st.radio('Selecciona el monitor a configurar:', ['Votos Históricos', 'Votos Potenciales', 'Líderes', 'Testigos', 'Potenciales Votantes', 'Votantes Previos'])
