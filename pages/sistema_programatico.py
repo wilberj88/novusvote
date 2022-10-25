2fdc5b83571e6729decd8eaaaef0a22ddc3781a1
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-25 07:23:39 -0500

    Update sistema_programatico.py

--- pages/sistema_programatico.py
+++ pages/sistema_programatico.py
@@ -7,3 +7,5 @@ st.set_page_config(layout="wide", page_title="Novus Vote", page_icon="🗳️")
 
 st.title('Novus Vote 🗳️')
 st.header("Sistema Inteligente de Gestión Programático")
+
+st.radio('Selecciona el monitor a configurar:', ['Necesidades', 'Sentimientos', 'Propuestas', 'Discursos'])
