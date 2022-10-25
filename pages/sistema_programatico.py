a7a65f994381663639b7046aaf357e1e2d3b9c7c
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-25 07:16:41 -0500

    Create sistema_programatico.py

---
+++ pages/sistema_programatico.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Vote", page_icon="🗳️")
+
+st.title('Novus Vote 🗳️')
+st.header("Sistema Inteligente de Gestión Programático")
