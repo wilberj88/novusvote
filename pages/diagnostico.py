04f3d5203ccda6e1d5ab47e936cf7cf4d56ada85
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-11-01 13:00:02 -0500

    Update diagnostico.py

--- pages/diagnostico.py
+++ pages/diagnostico.py
@@ -92,56 +92,48 @@ if st.button('Calcular diagnóstico gratuito'):
     columns=["a", "b", "c"])
     st.bar_chart(chart_data)
 
-    
-
-
-
-
-
-
-
-df = pd.DataFrame(
+    df = pd.DataFrame(
     np.random.randn(200, 3),
     columns=['a', 'b', 'c'])
 
-c = alt.Chart(df).mark_circle().encode(
+    c = alt.Chart(df).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
-st.altair_chart(c, use_container_width=True)
+    st.altair_chart(c, use_container_width=True)
 
 # Data src:  https://www.kaggle.com/manohar676/hotel-reviews-segmentation-recommended-system
 # Credit to: Manohar Reddy
-df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Plotly_Graphs/Radar-chart/ExistingHotels_CustomerVisitsdata-1554810038262.csv")
-df = df[df['Hotelid'].isin(['hotel_101','hotel_102','hotel_103'])]
-print(df.iloc[:20, :8])
+        df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Plotly_Graphs/Radar-chart/ExistingHotels_CustomerVisitsdata-1554810038262.csv")
+        df = df[df['Hotelid'].isin(['hotel_101','hotel_102','hotel_103'])]
+        print(df.iloc[:20, :8])
 
-df = df.groupby('Hotelid')[['Cleanliness_rating', 'Service_rating', 'Value_rating',
-                            'Rooms_rating','Checkin_rating',
-                            'Businessservice_rating']].mean().reset_index()
-print(df)
+        df = df.groupby('Hotelid')[['Cleanliness_rating', 'Service_rating', 'Value_rating',
+                                    'Rooms_rating','Checkin_rating',
+                                    'Businessservice_rating']].mean().reset_index()
+        print(df)
 
 # Convert from wide data to long data to plot radar chart
-df = pd.melt(df, id_vars=['Hotelid'], var_name='category', value_name='rating',
-             value_vars=['Cleanliness_rating', 'Service_rating', 'Value_rating',
-                         'Rooms_rating','Checkin_rating','Businessservice_rating'],
-)
-print(df)
+        df = pd.melt(df, id_vars=['Hotelid'], var_name='category', value_name='rating',
+                     value_vars=['Cleanliness_rating', 'Service_rating', 'Value_rating',
+                                 'Rooms_rating','Checkin_rating','Businessservice_rating'],
+        )
+        print(df)
 
 # radar chart Plotly examples - https://plotly.com/python/radar-chart/
 # radar chart Plotly docs = https://plotly.com/python-api-reference/generated/plotly.express.line_polar.html#plotly.express.line_polar
-fig = px.line_polar(df, r='rating', theta='category', color='Hotelid', line_close=True,
-                            line_shape='linear',  # or spline
-                    hover_name='Hotelid',
-                    hover_data={'Hotelid':False},
-                    markers=True,
-                    # labels={'rating':'stars'},
-                    # text='Hotelid',
-                    # range_r=[0,10],
-                    direction='clockwise',  # or counterclockwise
-                    start_angle=45
-                    )
-# fig.update_traces(fill='toself')
-fig.show()
+        fig = px.line_polar(df, r='rating', theta='category', color='Hotelid', line_close=True,
+                                    line_shape='linear',  # or spline
+                            hover_name='Hotelid',
+                            hover_data={'Hotelid':False},
+                            markers=True,
+                            # labels={'rating':'stars'},
+                            # text='Hotelid',
+                            # range_r=[0,10],
+                            direction='clockwise',  # or counterclockwise
+                            start_angle=45
+                            )
+        # fig.update_traces(fill='toself')
+        fig.show()
 
 
 
