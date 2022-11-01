8afddc41e0f2cce7bfcffbbfe138800c7166dbed
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-31 19:26:05 -0500

    Update diagnostico.py

--- pages/diagnostico.py
+++ pages/diagnostico.py
@@ -38,6 +38,34 @@ with col2:
 if territorio and categoria and redsocial and perfil and meta:  
     st.header("Requerimientos de Campaña")
     st.write("Votos necesarios de acuerdo con las votaciones históricas")
+    st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=df,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=df,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
     st.write("Líderes necesarios")
     chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
     st.area_chart(chart_data)
