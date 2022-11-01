06e01f0fbff58b923abbfb2a3419201426f3104c
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-11-01 13:01:22 -0500

    Update diagnostico.py

--- pages/diagnostico.py
+++ pages/diagnostico.py
@@ -103,25 +103,25 @@ if st.button('Calcular diagnóstico gratuito'):
 
 # Data src:  https://www.kaggle.com/manohar676/hotel-reviews-segmentation-recommended-system
 # Credit to: Manohar Reddy
-        df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Plotly_Graphs/Radar-chart/ExistingHotels_CustomerVisitsdata-1554810038262.csv")
-        df = df[df['Hotelid'].isin(['hotel_101','hotel_102','hotel_103'])]
-        print(df.iloc[:20, :8])
+    df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Plotly_Graphs/Radar-chart/ExistingHotels_CustomerVisitsdata-1554810038262.csv")
+    df = df[df['Hotelid'].isin(['hotel_101','hotel_102','hotel_103'])]
+    print(df.iloc[:20, :8])
 
-        df = df.groupby('Hotelid')[['Cleanliness_rating', 'Service_rating', 'Value_rating',
+    df = df.groupby('Hotelid')[['Cleanliness_rating', 'Service_rating', 'Value_rating',
                                     'Rooms_rating','Checkin_rating',
                                     'Businessservice_rating']].mean().reset_index()
-        print(df)
+    print(df)
 
 # Convert from wide data to long data to plot radar chart
-        df = pd.melt(df, id_vars=['Hotelid'], var_name='category', value_name='rating',
+    df = pd.melt(df, id_vars=['Hotelid'], var_name='category', value_name='rating',
                      value_vars=['Cleanliness_rating', 'Service_rating', 'Value_rating',
                                  'Rooms_rating','Checkin_rating','Businessservice_rating'],
         )
-        print(df)
+    print(df)
 
 # radar chart Plotly examples - https://plotly.com/python/radar-chart/
 # radar chart Plotly docs = https://plotly.com/python-api-reference/generated/plotly.express.line_polar.html#plotly.express.line_polar
-        fig = px.line_polar(df, r='rating', theta='category', color='Hotelid', line_close=True,
+    fig = px.line_polar(df, r='rating', theta='category', color='Hotelid', line_close=True,
                                     line_shape='linear',  # or spline
                             hover_name='Hotelid',
                             hover_data={'Hotelid':False},
@@ -133,7 +133,7 @@ if st.button('Calcular diagnóstico gratuito'):
                             start_angle=45
                             )
         # fig.update_traces(fill='toself')
-        fig.show()
+    fig.show()
 
 
 
