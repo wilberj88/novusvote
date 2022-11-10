1e26cb4076a473a09dfec1c423fd2cc444d69562
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-11-10 11:13:36 -0500

    Update campaña.py

--- pages/campaña.py
+++ pages/campaña.py
@@ -10,13 +10,6 @@ import datetime
 import base64
 
 # SETTING BACKGROUND
-@st.experimental_memo
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-img = get_img_as_base64("/img1NovusVote.jpg")
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
@@ -27,7 +20,7 @@ background-repeat: no-repeat;
 background-attachment: local;
 }}
 [data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
+background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
 background-position: center; 
 background-repeat: no-repeat;
 background-attachment: fixed;
