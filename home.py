5b0d54095cc0a2591423d6840e2e090c753cc261
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-11-30 08:53:59 -0500

    Update home.py

--- home.py
+++ home.py
@@ -44,3 +44,15 @@ acelerometro = {
     }
 
 st_echarts(options=acelerometro)
+
+options = {
+    "xAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "yAxis": {"type": "value"},
+    "series": [
+        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
+    ],
+}
+st_echarts(options=options)
