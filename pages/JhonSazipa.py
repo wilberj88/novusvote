b1e0485fabb52b7dd8eadf5bbcc4d866fbce494f
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-03 11:31:32 -0500

    Update JhonSazipa.py

--- pages/JhonSazipa.py
+++ pages/JhonSazipa.py
@@ -88,40 +88,40 @@ with col3:
 
   
 st.write('Diagnóstico y Contraste de Competencia Actual y Previa')
-
-
 def render_basic_radar():
     option = {
-        "title": {"text": "基础雷达图"},
-        "legend": {"data": ["预算分配（Allocated Budget）", "实际开销（Actual Spending）"]},
+        "title": {"text": "Comparaciones"},
+        "legend": {"data": ["Candidato A", "Candidato B"]},
         "radar": {
             "indicator": [
-                {"name": "销售（Sales）", "max": 6500},
-                {"name": "管理（Administration）", "max": 16000},
-                {"name": "信息技术（Information Technology）", "max": 30000},
-                {"name": "客服（Customer Support）", "max": 38000},
-                {"name": "研发（Development）", "max": 52000},
-                {"name": "市场（Marketing）", "max": 25000},
+                {"name": "Líderes", "max": 6500},
+                {"name": "Financiación", "max": 16000},
+                {"name": "Sentimiento", "max": 30000},
+                {"name": "Votación Anterior", "max": 38000},
+                {"name": "Interaciones", "max": 52000},
+                {"name": "Recordación de Marca", "max": 25000},
             ]
         },
         "series": [
             {
-                "name": "预算 vs 开销（Budget vs spending）",
+                "name": "Aprendizaje Actual Vs Proyectado",
                 "type": "radar",
                 "data": [
                     {
-                        "value": [4200, 3000, 20000, 35000, 50000, 18000],
-                        "name": "预算分配（Allocated Budget）",
+                        "value": [2000, 3000, 2000, 3500, 15000, 11800],
+                        "name": "Estado Actual de Alumno",
                     },
                     {
-                        "value": [5000, 14000, 28000, 26000, 42000, 21000],
-                        "name": "实际开销（Actual Spending）",
+                        "value": [6500, 16000, 30000, 38000, 52000, 25000],
+                        "name": "Meta de Aprendizaje",
                     },
                 ],
             }
         ],
     }
     st_echarts(option, height="500px")
+
+
 
 
 ST_RADAR_DEMOS = {
