d0d0c71d6aba48767073c80d92641baa1348899d
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-05-08 20:23:41 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,17 @@
 import streamlit as st
 from streamlit_echarts import st_echarts
+from streamlit_echarts import st_pyecharts
+import plotly.express as px
 import pandas as pd
 import numpy as np
+import plotly.figure_factory as ff
+import altair as alt
+import pydeck as pdk
+import matplotlib.pyplot as plt
+import datetime
+import base64
 from pyecharts import options as opts
-from pyecharts.charts import Bar
-from streamlit_echarts import st_pyecharts
+from pyecharts.charts import Graph
 
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
