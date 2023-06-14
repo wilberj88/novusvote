611013ddf78687d0cb6894f5a52f6817613d4de6
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-06-14 15:04:42 -0500

    Update home.py

--- home.py
+++ home.py
@@ -107,3 +107,65 @@ def run_code():
 
 
 run_code()
+
+
+
+st.header("Cronograma de Interacciones por Equipos")
+options = {
+    "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+    "legend": {
+        "data": ["Líderes", "Mail Ad", "Affiliate Ad", "Video Ad", "Search Engine"]
+    },
+    "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+    "xAxis": {"type": "value"},
+    "yAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "series": [
+        {
+            "name": "Líderes",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [320, 302, 301, 334, 390, 330, 320],
+        },
+        {
+            "name": "Mail Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+            "name": "Affiliate Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+            "name": "Video Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [150, 212, 201, 154, 190, 330, 410],
+        },
+        {
+            "name": "Search Engine",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [820, 832, 901, 934, 1290, 1330, 1320],
+        },
+    ],
+}
+st_echarts(options=options, height="500px")
+          
+          
+          
