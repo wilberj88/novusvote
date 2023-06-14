a42b5f41bc03704114f60dff95c90a031c2fd9dd
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-06-14 16:10:22 -0500

    Update home.py

--- home.py
+++ home.py
@@ -598,7 +598,7 @@ option = {
             "data": [
                 {"value": 30, "name": "访问"},
                 {"value": 10, "name": "咨询"},
-                {"value": 5, "name": "订单"},
+                {"value": 5, "name": "Votantes"},
                 {"value": 50, "name": "点击"},
                 {"value": 80, "name": "展现"},
             ],
