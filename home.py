c1924856dd4dfb1998b4d7f1acc0a6908cac2c11
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-06-14 11:46:46 -0500

    Update home.py

--- home.py
+++ home.py
@@ -34,11 +34,11 @@ items = [
 ]
 
 #items = [
-    {"id": 1, "PRE Campaña inicia:": "2023-06-15", "PRE Campaña finaliza:": "2023-07-29"},
-    {"id": 2, "Campaña inicia:": "2023-07-29", "Campaña finaliza": "2023-10-21"},
-    {"id": 3, "Día E inicia:": "2023-10-22", "Día E finalizada:": "2023-11-06"},
-    {"id": 4, "POST Campaña inicia": "2022-10-16", "POST Campaña finaliza": "2022-10-16"},
-]
+#    {"id": 1, "PRE Campaña inicia:": "2023-06-15", "PRE Campaña finaliza:": "2023-07-29"},
+#    {"id": 2, "Campaña inicia:": "2023-07-29", "Campaña finaliza": "2023-10-21"},
+#    {"id": 3, "Día E inicia:": "2023-10-22", "Día E finalizada:": "2023-11-06"},
+#    {"id": 4, "POST Campaña inicia": "2022-10-16", "POST Campaña finaliza": "2022-10-16"},
+#]
 
 timeline = st_timeline(items, groups=[], options={}, height="300px")
 st.subheader("Selected item")
