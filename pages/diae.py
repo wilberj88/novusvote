e016b8890b48c136558ce79b3644cc2c6ef325a0
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-06-15 16:03:09 -0500

    Update diae.py

--- pages/diae.py
+++ pages/diae.py
@@ -53,6 +53,43 @@ st.write(timeline)
 
 #SUBTITULO
 st.write('---')
+st.header("Tareas completadas por los equipos")
+option = st.selectbox(
+    'Selecciona un barrio',
+    ('Barrio1', 'Barrio2', 'Barrio3'))
+df = pd.DataFrame(
+np.random.randn(1000, 2) / [50, 50] + [6.15333, -75.374166],
+columns=['lat', 'lon'])
+st.pydeck_chart(pdk.Deck(
+map_style=None,
+initial_view_state=pdk.ViewState(
+latitude=6.153333,
+longitude=-75.374166,
+zoom=11,
+pitch=50,
+),
+layers=[
+pdk.Layer(
+   'HexagonLayer',
+   data=df,
+   get_position='[lon, lat]',
+   radius=200,
+   elevation_scale=4,
+   elevation_range=[0, 1000],
+   pickable=True,
+   extruded=True,
+),
+pdk.Layer(
+    'ScatterplotLayer',
+    data=df,
+    get_position='[lon, lat]',
+    get_color='[200, 30, 0, 160]',
+    get_radius=200,
+),
+],
+))
+
+
 st.write("""
 **Cinco (5) Centrales de Mando para:**
 - 🤔: `Paradoja por Perfiles de Votantes`
