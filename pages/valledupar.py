98a47afc9c607a1b67bb4caa2aab2871a7e2db62
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-03 11:56:51 -0500

    Update valledupar.py

--- pages/valledupar.py
+++ pages/valledupar.py
@@ -19,10 +19,57 @@ from pyecharts.charts import Graph
 st.set_page_config(layout="wide", page_title="Novus Vote 🗳️ Valledupar PRE CAMPAÑA", page_icon="🗳️")
 
 #TITULO
-st.title('Novus Vote 🗳️ - Valledupar PRE CAMPAÑA V.0.5')
+st.title('Novus Vote 🗳️ - Valledupar PRE CAMPAÑA V.0.6')
 
 #META
 meta = 70000
 st.write('Votos mínimos requeridos para aspirar a posesionarse Alcalde de Valledupar:', meta) 
 
-st.title("Desagregación de votos en 2019 por mesa de votación, barrios y zonas")
+st.title("Desagregación de votos válidos en 2015 por candidaturas y zonas")
+def render_basic_radar():
+    option = {
+        "title": {"text": "Votación 2015 Alcaldía Valledupar 🗳️"},
+        "legend": {"data": ["Ganador 2015", "2do 2015", "3ro 2015"]},
+        "radar": {
+            "indicator": [
+                {"name": "Zona 1", "max": 14000},
+                {"name": "Zona 2", "max": 14000},
+                {"name": "Zona 3", "max": 14000},
+                {"name": "Zona 4", "max": 14000},
+                {"name": "Zona 5", "max": 14000},
+                {"name": "Zona 6", "max": 14000},
+                {"name": "Zona 7", "max": 14000},
+                {"name": "Zona 8", "max": 14000},
+                {"name": "Zona 9", "max": 14000},
+            ]
+        },
+        "series": [
+            {
+                "name": "Votos por Zonas",
+                "type": "radar",
+                "data": [
+                    {
+                        "value": [10898, 12260, 10461, 13667, 11792, 4659, 4364, 152, 5931],
+                        "name": "Ganador 2015",
+                    },
+                    {
+                        "value": [7055, 6004, 5288, 6842, 7077, 4127, 2267, 58, 3337],
+                        "name": "2do 2015",
+                    },
+                    {
+                        "value": [2273, 2792, 2419, 2954, 2437, 974, 916, 16, 2863],
+                        "name": "3ro 2015",
+                    },
+                ],
+            }
+        ],
+    }
+    st_echarts(option, height="500px")
+ST_RADAR_DEMOS = {
+    "Radar: Basic Radar": (
+        render_basic_radar,
+        "https://echarts.apache.org/examples/en/editor.html?c=radar",
+    ),
+}
+render_basic_radar()
+st.title("Desagregación de votos válidos en 2019 por candidaturas y zonas")
