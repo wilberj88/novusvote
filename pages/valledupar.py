155aea64f7dde9ebb72f796c8c1a107f11420873
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-05 15:52:30 -0500

    Update valledupar.py

--- pages/valledupar.py
+++ pages/valledupar.py
@@ -215,20 +215,19 @@ st_echarts(
 
 st.write("Rango de votos requeridos para ganar Alcaldía 2023 (39,7 - 48,5%)")
 schools = ["Brown", "NYU", "Notre Dame", "Cornell", "Tufts", "Yale",
-           "Dartmouth", "Chicago", "Columbia", "Duke", "Georgetown",
-           "Princeton", "U.Penn", "Stanford", "MIT", "Harvard"]
+           "Dartmouth", "Chicago", "Columbia"]
 n_schools = len(schools)
 
-women_salary = [72, 67, 73, 80, 76, 79, 84, 78, 86, 93, 94, 90, 92, 96, 94, 112]
-men_salary = [92, 94, 100, 107, 112, 114, 114, 118, 119, 124, 131, 137, 141, 151, 152, 165]
+women_salary = [72, 67, 73, 80, 76, 79, 84, 78, 65]
+men_salary = [92, 94, 100, 107, 112, 114, 114, 118, 119]
 
 df = pd.DataFrame(dict(school=schools*2, salary=men_salary + women_salary,
-                       gender=["Men"]*n_schools + ["Women"]*n_schools))
+                       gender=["Bajo"]*n_schools + ["Alto"]*n_schools))
 
 # Use column names of df for the different parameters x, y, color, ...
 fig = px.scatter(df, x="salary", y="school", color="gender",
-                 title="Gender Earnings Disparity",
-                 labels={"salary":"Annual Salary (in thousands)"} # customize axis label
+                 title="Entre el 39,7% y el 48,5%",
+                 labels={"salary":"Votos válidos mínimos para ser Alcalde Valledupar en 2023"} # customize axis label
                 )
 st.plotly_chart(fig, theme="streamlit")
 st.write('---')
