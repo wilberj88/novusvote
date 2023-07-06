e13d1957921a881bb616839a9fdbafeae1aff10e
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 17:15:19 -0500

    Update avatares.py

--- pages/avatares.py
+++ pages/avatares.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import graphviz
+import random
+import time
 
 st.set_page_config(layout="wide", page_title="Novus Vote 🗳️ Tecnología Electoral", page_icon="🗳️")
 
@@ -23,3 +25,41 @@ graph.edge('ReporteDíaE', 'Reporte y Ranking TOTAL')
 
 
 st.graphviz_chart(graph)
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["user"]):
+        st.markdown(message["👋Bienvenido a esta Campaña Electoral: dime ¿cuál equipo deseas apoyar?"])
+# React to user input
+if prompt := st.chat_input("Dímelo de una"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Excelente, bienvenido ",
+                "Hi, human! Is there anything I can help you with?",
+                "Do you need help?",
+            ]
+        )
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.1)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    
