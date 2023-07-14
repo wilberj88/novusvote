0da092990b09d1a0a1fbe16bbf7ffb79fcfcf5a1
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-14 10:38:44 -0500

    Update valledupar.py

--- pages/valledupar.py
+++ pages/valledupar.py
@@ -266,6 +266,40 @@ pdk.Layer(
 
 st.write('---')
 
+st.title("Predicción de votos válidos en Alcaldía Valledupar 2023 por puesto de votación y zonas")
+#datos
+df = pd.DataFrame(
+np.random.randn(1000, 2) / [50, 50] + [10.473583, -73.248639],
+columns=['lat', 'lon'])
+st.pydeck_chart(pdk.Deck(
+map_style=None,
+initial_view_state=pdk.ViewState(
+latitude=10.473583,
+longitude=-73.248639,
+zoom=11,
+pitch=50,
+),
+layers=[
+pdk.Layer(
+   'HexagonLayer',
+   data=df,
+   get_position='[lon, lat]',
+   radius=200,
+   elevation_scale=4,
+   elevation_range=[0, 1000],
+   pickable=True,
+   extruded=True,
+),
+pdk.Layer(
+    'ScatterplotLayer',
+    data=df,
+    get_position='[lon, lat]',
+    get_color='[200, 30, 0, 160]',
+    get_radius=200,
+),
+],
+))
+st.write('---')
 
 st.header('Conclusiones PRE CAMPAÑA - Tareas Campaña')
 meta = 84558
