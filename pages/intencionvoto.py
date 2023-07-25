60068581199aa495c5f56eae1224a9e03cc29267
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-25 10:56:39 -0500

    Update intencionvoto.py

--- pages/intencionvoto.py
+++ pages/intencionvoto.py
@@ -19,3 +19,47 @@ st.set_page_config(layout="wide", page_title="Novus Vote 🗳️", page_icon="�
 
 #TITULO
 st.title('Novus Vote 🗳️ - Modelación Intención de Voto V.0.1')
+st.header("Desde la Escucha Social hasta el Puntaje de Porcentaje de Votación Válida")
+
+
+st.header("Escucha Social")
+st.write("Desagregada por Redes Sociales")
+options = {
+    "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+    "legend": {
+        "data": ["Sentimiento Positivo", "Sentimiento Negativo", "Sentimiento Neutro"]
+    },
+    "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+    "xAxis": {"type": "value"},
+    "yAxis": {
+        "type": "category",
+        "data": ["Red Social 1", "Red Social 2", "Red Social 3", "Red Social 4", "Red Social 5", "Red Social 6", "Red Social 7", "Red Social 8", "Red Social 9"],
+    },
+    "series": [
+        {
+            "name": "Sentimiento Positivo",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [10898, 12260, 10461, 13667, 11792, 4659, 4364, 152, 5931],
+        },
+        {
+            "name": "Sentimiento Negativo",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [7055, 6004, 5288, 6842, 7077, 4127, 2267, 58, 3337],
+        },
+        {
+            "name": "Sentimiento Neutro",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [2273, 2792, 2419, 2954, 2437, 974, 916, 16, 2863],
+        },
+    ],
+}
+st_echarts(options=options, height="500px")
