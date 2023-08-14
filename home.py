86379d2c80bb89c4f2627bd4fce433372e701afd
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-08-14 16:32:21 -0500

    Update home.py

--- home.py
+++ home.py
@@ -43,19 +43,14 @@ with tab4:
    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
 
-items = [
-    {"id": 1, "content": "2023-06-15", "start": "2023-07-29"},
-    {"id": 2, "content": "2023-10-21", "start": "2023-07-30"},
-    {"id": 3, "content": "2023-11-06", "start": "2023-10-22"},
-    {"id": 4, "content": "2023-11-07", "start": "2024-01-30"},
-]
 
-#items = [
-#    {"id": 1, "PRE Campaña inicia:": "2023-06-15", "PRE Campaña finaliza:": "2023-07-29"},
-#    {"id": 2, "Campaña inicia:": "2023-07-29", "Campaña finaliza": "2023-10-21"},
-#    {"id": 3, "Día E inicia:": "2023-10-22", "Día E finalizada:": "2023-11-06"},
-#    {"id": 4, "POST Campaña inicia": "2022-10-16", "POST Campaña finaliza": "2022-10-16"},
-#]
+
+items = [
+    {"id": 1, "PRE Campaña inicia:": "2023-06-15", "PRE Campaña finaliza:": "2023-07-29"},
+    {"id": 2, "Campaña inicia:": "2023-07-29", "Campaña finaliza": "2023-10-21"},
+    {"id": 3, "Día E inicia:": "2023-10-22", "Día E finalizada:": "2023-11-06"},
+    {"id": 4, "POST Campaña inicia": "2022-10-16", "POST Campaña finaliza": "2022-10-16"},
+]
 
 timeline = st_timeline(items, groups=[], options={}, height="300px")
 st.subheader("Selected item")
