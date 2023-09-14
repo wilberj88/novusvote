20b09d3151e5c3df46474cbdd442f92f35ab9687
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-09-14 16:22:59 -0500

    Create cundinamarca.py

---
+++ pages/cundinamarca.py
@@ -0,0 +1,181 @@
+import streamlit as st
+from streamlit_echarts import st_echarts
+from streamlit_echarts import st_pyecharts
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import altair as alt
+import pydeck as pdk
+import matplotlib.pyplot as plt
+import datetime
+import base64
+import graphviz
+from pyecharts import options as opts
+from pyecharts.charts import Graph
+import folium
+from streamlit_folium import st_folium
+import time
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Vote 🗳️ Cundinamarca", page_icon="🗳️")
+
+#TITULO
+st.title('Novus Vote 🗳️ - Gobernación Cundinamarca 2023')
+
+st.title('Central de PRE CAMPAÑA')
+st.subheader(':red[Votación Histórica 2015-2019], :blue[Proyecciones 2023] y :green[Requisitos para Ganar] 🏆')
+st.header("1. Históricos: Datos procesados 🛠️")
+
+st.subheader("Votación Gobernación 2015")
+
+st.subheader("Votación Gobernación 2019")
+
+st.write("Resultados E-14 por Zonas")
+options = {
+    "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+    "legend": {
+        "data": ["Ganador_Ramirez", "2do_Gonzalez", "3ro_Fernandez"]
+    },
+    "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+    "xAxis": {"type": "value"},
+    "yAxis": {
+        "type": "category",
+        "data": ["Zona 1", "Zona 2", "Zona 3", "Zona 4", "Zona 5", "Zona 6", "Zona 7", "Zona 8", "Zona 9"],
+    },
+    "series": [
+        {
+            "name": "Ganador_Ramirez",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [10898, 12260, 10461, 13667, 11792, 4659, 4364, 152, 5931],
+        },
+        {
+            "name": "2do_Gonzalez",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [7055, 6004, 5288, 6842, 7077, 4127, 2267, 58, 3337],
+        },
+        {
+            "name": "3ro_Fernandez",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [2273, 2792, 2419, 2954, 2437, 974, 916, 16, 2863],
+        },
+    ],
+}
+st_echarts(options=options, height="500px")
+
+st.header("Votación Alcaldía 2019")
+st.write("Resultados E-14 por Zonas")
+options = {
+    "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+    "legend": {
+        "data": ["Ganador_Castro", "2do_Orozco", "3ro_Morales"]
+    },
+    "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+    "xAxis": {"type": "value"},
+    "yAxis": {
+        "type": "category",
+        "data": ["Zona 1", "Zona 2", "Zona 3", "Zona 4", "Zona 5", "Zona 6", "Zona 7", "Zona 8", "Zona 9"],
+    },
+    "series": [
+        {
+            "name": "Ganador_Castro",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [9549, 11314, 10390, 12513, 12728, 4977, 2935, 84, 8905],
+        },
+        {
+            "name": "2do_Orozco",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [10013, 9513, 9712, 11971, 12824, 5638, 2562, 80, 7957],
+        },
+        {
+            "name": "3ro_Morales",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [2055, 4196, 4228, 4887, 3776, 678, 1143, 39, 528],
+        },
+    ],
+}
+st_echarts(options=options, height="500px")
+
+
+st.header("Top 3 entre 2015 y 2019")
+def render_basic_radar():
+    option = {
+        "title": {"text": "Zonas Alcaldía Valledupar 🗳️"},
+        "legend": {"data": ["Ganador 2015", "2do 2015", "3ro 2015", "Ganador 2019", "2do 2019", "3ro 2019"]},
+        "radar": {
+            "indicator": [
+                {"name": "Zona 1", "max": 14000},
+                {"name": "Zona 2", "max": 14000},
+                {"name": "Zona 3", "max": 14000},
+                {"name": "Zona 4", "max": 14000},
+                {"name": "Zona 5", "max": 14000},
+                {"name": "Zona 6", "max": 14000},
+                {"name": "Zona 7", "max": 14000},
+                {"name": "Zona 8", "max": 14000},
+                {"name": "Zona 9", "max": 14000},
+            ]
+        },
+        "series": [
+            {
+                "name": "Votos por Zonas",
+                "type": "radar",
+                "data": [
+                    {
+                        "value": [10898, 12260, 10461, 13667, 11792, 4659, 4364, 152, 5931],
+                        "name": "Ganador 2015",
+                    },
+                    {
+                        "value": [7055, 6004, 5288, 6842, 7077, 4127, 2267, 58, 3337],
+                        "name": "2do 2015",
+                    },
+                    {
+                        "value": [2273, 2792, 2419, 2954, 2437, 974, 916, 16, 2863],
+                        "name": "3ro 2015",
+                    },
+                    {
+                        "value": [9549, 11314, 10390, 12513, 12728, 4977, 2935, 84, 8095],
+                        "name": "Ganador 2019",
+                    },
+                    {
+                        "value": [10013, 9513, 9712, 11971, 12824, 5638, 2562, 80, 7957],
+                        "name": "2do 2019",
+                    },
+                    {
+                        "value": [2055, 4196, 4228, 4887, 3776, 678, 1143, 39, 528],
+                        "name": "3ro 2019",
+                    },
+                ],
+            }
+        ],
+    }
+    st_echarts(option, height="500px")
+ST_RADAR_DEMOS = {
+    "Radar: Basic Radar": (
+        render_basic_radar,
+        "https://echarts.apache.org/examples/en/editor.html?c=radar",
+    ),
+}
+render_basic_radar()
+
+st.header('¿Qué dicen los datos históricos?')
+st.write('1) Porcentaje mínimo para posesionarse Alcalde en Valledupar ha sido el 39%')
+st.write('2) Porcentaje promedio de crecimiento de los votos válidos está en el 18%')
+st.write('3) De las 7 zonas de la ciudad, las 5 primeras arrojan el 80% de la votación, siendo las zonas 4 y 5 las más grandes con 18,1% de participación cada una')
