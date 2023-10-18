de9c5bb319aca715ac354042b8f987b9a14e30f6
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-10-18 03:27:33 -0500

    Update valledupar.py

--- pages/valledupar.py
+++ pages/valledupar.py
@@ -32,12 +32,10 @@ data = pd.read_csv('pages/datos/Votos Válidos procesados Valledupar 2015-2019 
 data_pura = data.dropna()
 
 with st.expander("Ver explicación de los datos usados:"):
-    st.write("
-         FUENTES DE LOS DATOS:
-         Votos y abstención: Formularios E-14 de la Registraduría General de la Nación.
-         Ubicación: Latitudes y Longitudes de Google Maps
-         Demografía: DANE.
-    ")
+    st.write('FUENTES DE LOS DATOS')
+    st.write('Votos y abstención: Formularios E-14 de la Registraduría General de la Nación.')
+    st.write('Ubicación: Latitudes y Longitudes de Google Maps')     
+    st.write('Demografía: DANE')
     st.dataframe(data_pura)
 
 st.header("Votación Alcaldía 2015")
