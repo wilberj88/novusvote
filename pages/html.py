553e125e28f4075662c6d340e8c8f94526dcecaa
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-23 23:58:05 +0100

    Create html.py

---
+++ pages/html.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as com
+com.html("""
+<form action="https://formsubmit.co/your@email.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
+</form>
+""")
