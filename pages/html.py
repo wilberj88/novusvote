2fcc7c208d81c8a4aa8a4ac7c1dce1f48fb3af5e
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-24 00:02:38 +0100

    Update html.py

--- pages/html.py
+++ pages/html.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import streamlit.components.v1 as com
 com.html("""
-<form action="https://formsubmit.co/your@email.com" method="POST">
+<div>
+<h1>
+Conctáctanos
+</h1>
+<form action="https://formsubmit.co/wilber.jimenez.hdz@gmail.com" method="POST">
      <input type="text" name="name" required>
      <input type="email" name="email" required>
      <button type="submit">Send</button>
 </form>
+</div>
 """)
